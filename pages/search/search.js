a932a923321741f9922e7e963d2b85410a8e1862
GuanghuiYear <956375089@qq.com>
2019-07-08 14:21:00 +0800

    Signed-off-by: GuanghuiYear <956375089@qq.com>

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,36 +1,39 @@
-var app=getApp();
+var app = getApp();
 Page({
   data: {
     searchVal: '',
     searchResult: '',
-    describe:'',
-    hasResult:false
+    describe: '',
+    hasResult: false
   },
   onLoad: function(options) {
-    // console.log(options);
-    let that = this;
-    this.setData({
-      searchVal: options.search
-    });
-    wx.request({
-      url: app.globalData.baseUrl + '/api/rubbish_sort?keywords=' + options.search,
-      success: function(res) {
-        that.setData({
-          hasResult:true
-        })
-        console.log(res, res.data.data[0])
-        if (res.data.code == 0) {
+    console.log(options);
+    if (options.length>0) {
+      let that = this;
+      this.setData({
+        searchVal: options.search
+      });
+      wx.request({
+        url: app.globalData.baseUrl + '/api/rubbish_sort?keywords=' + options.search,
+        success: function(res) {
           that.setData({
-            searchResult: res.data.data[0].type
+            hasResult: true
           })
-        } else {
-          that.setData({
-            searchResult: '外星物种',
-            describe:'是指我无法识别的物种、主要包括我的大脑中没有记忆的未来生物，你好厉害！'
-          })
-        }        
-      }
-    })
+          console.log(res, res.data.data[0])
+          if (res.data.code == 0) {
+            that.setData({
+              searchResult: res.data.data[0].type
+            })
+          } else {
+            that.setData({
+              searchResult: '外星物种',
+              describe: '是指我无法识别的物种、主要包括我的大脑中没有记忆的未来生物，你好厉害！'
+            })
+          }
+        }
+      })
+    }
+
   },
   toKnowledge() {
     wx.navigateTo({
