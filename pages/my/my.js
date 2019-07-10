8f70c9c946d85082e7ea093501971bafb7baa942
GuanghuiYear <956375089@qq.com>
2019-07-10 10:25:56 +0800

    Signed-off-by: GuanghuiYear <956375089@qq.com>

--- pages/my/my.js
+++ pages/my/my.js
@@ -2,35 +2,22 @@ var util = require('../../utils/util.js');
 var app = getApp();
 Page({
   data: {
-    hasUserInfo: false,
     nickName: '小卫士',
-    avatarUrl: '/images/head.png'
+    avatarUrl: '/images/head.png',
+    is_login: false
   },
   onLoad: function(options) {
-
-    var that = this;
-    wx.getUserInfo({
-      success: function(res) {
-        console.log(res);
-        that.setData({
-          nickName: res.userInfo.nickName,
-          avatarUrl: res.userInfo.avatarUrl,
-        })
-      },
-    })
+    this.myInfo();
   },
-
   register(result) {
     if (result.detail.errMsg == 'getUserInfo:ok') {
       var that = this;
       wx.login({
         success: function(t) {
           let code = t.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
-
-
           util.httpPost(getApp().globalData.baseUrl + '/login/UpdateUserInfo', {
               code: code,
-              openId: getApp().globalData.openId,
+              openId: app.globalData.openId,
               encryptedData: result.detail.encryptedData,
               iv: result.detail.iv,
               signature: result.detail.signature,
@@ -43,7 +30,7 @@ Page({
                   content: res.info,
                   showCancel: false,
                   success: function() {
-                    // that.userCenter();
+                    that.myInfo();
                   }
                 })
               } else {
@@ -59,6 +46,22 @@ Page({
         }
       })
     }
-  }
+  },
+  myInfo: function () {
+    var that = this;
+    util.httpPost(getApp().globalData.baseUrl + '/User/index', {
+      openid: getApp().globalData.openId
+    }, function (res) {
+      if (res.code == 0) {
+        if (res.userInfo.head_pic) {
+          that.setData({
+            nickName: res.userInfo.nickname,
+            avatarUrl: res.userInfo.head_pic,
+            is_login: true
+          })
+        }
+      }
+    })
+  },
 
 })
